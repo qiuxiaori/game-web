5bef7720c9969eab36dc5a45c59eaadf07c950bd
qiuxiaori <zmy811211534@163.com>
2020-03-20 14:10:40 +0800

    g

--- src/main.js
+++ src/main.js
@@ -3,18 +3,13 @@ import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 import App from './App'
 import axios from 'axios'
-import VueRouter from 'vue-router'
 import VueAxios from 'vue-axios'
-import myrouter from './routers/router'
 Vue.use(VueAxios, axios)
 Vue.use(Antd)
-Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
 
-const router = new VueRouter({ myrouter })
 new Vue({
   render: h => h(App),
-  router
 }).$mount('#app')
