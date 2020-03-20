bf9e3699ffc095a7dd88d28f48200fa55ec3d6a5
qiuxiaori <zmy811211534@163.com>
2020-03-20 14:51:47 +0800

    f

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import 'ant-design-vue/dist/antd.css'
 import App from './App'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import router from './router'
 Vue.use(VueAxios, axios)
 Vue.use(Antd)
 
@@ -11,5 +12,6 @@ Vue.config.productionTip = false
 Vue.prototype.$axios = axios
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
